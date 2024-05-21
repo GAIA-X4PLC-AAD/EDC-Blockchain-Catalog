cd45ddfbe19e8ca9f28c7df42533e01b765669aa
Julian Legler <julian.legler@campus.tu-berlin.de>
2024-05-21 16:29:10 +0200

    feat: add minimal working example using open telemetry and jaeger refactor: removed commented lines in build.gradle.kts refactor: docker files now use the opentelemetry java agent chore: add missing libs in libs.versions.toml
    Signed-off-by: Julian Legler <julian.legler@campus.tu-berlin.de>

--- BlockchainCatalog/blockchain-catalog-prosumer/build.gradle.kts
+++ BlockchainCatalog/blockchain-catalog-prosumer/build.gradle.kts
@@ -24,65 +24,6 @@ val groupId: String by project
 val edcVersion: String by project
 
 dependencies {
-    /*
-    implementation("$groupId:control-plane-core:$edcVersion")
-
-    implementation("$groupId:http:$edcVersion")
-
-    implementation("$groupId:configuration-filesystem:$edcVersion")
-    implementation("$groupId:iam-mock:$edcVersion")
-
-    implementation("$groupId:auth-tokenbased:$edcVersion")
-    implementation("$groupId:management-api:$edcVersion")
-    */
-    //implementation(project(":BlockchainCatalog:blockchain-catalog-api"))
-    //implementation(project(":BlockchainCatalog:blockchain-catalog-listener"))
-    //implementation(project(":blockchain-logger"))
-
-
-
-    /*
-
-
-    // data plane related dependencies to enable transfer via azurerite
-    implementation("$groupId:data-plane-azure-storage:$edcVersion")
-    implementation("$groupId:data-plane-util:$edcVersion")
-    implementation("$groupId:data-plane-core:$edcVersion")
-    implementation("$groupId:data-plane-framework:$edcVersion")
-    implementation("$groupId:data-plane-api:$edcVersion")
-    implementation("$groupId:control-plane-core:$edcVersion")
-    implementation("$groupId:data-plane-core:$edcVersion")
-    implementation("$groupId:data-plane-http:$edcVersion")
-    implementation("$groupId:data-plane-azure-storage:$edcVersion")
-    // implementation("$groupId:vault-azure:$edcVersion")
-    implementation("$groupId:transfer-data-plane:$edcVersion")
-    implementation("$groupId:transfer-core:$edcVersion")
-    implementation("$groupId:data-plane-client:$edcVersion")
-    implementation("$groupId:data-plane-selector-client:$edcVersion")
-    implementation("$groupId:data-plane-selector-core:$edcVersion")
-    implementation("$groupId:data-plane-selector-api:$edcVersion")
-    implementation("$groupId:transfer-data-plane:$edcVersion")
-
-    implementation("${groupId}:control-plane-core:${edcVersion}")
-    implementation("${groupId}:configuration-filesystem:${edcVersion}")
-    implementation("${groupId}:vault-filesystem:${edcVersion}")
-    implementation("${groupId}:iam-mock:${edcVersion}")
-    implementation("${groupId}:management-api:${edcVersion}")
-    implementation("${groupId}:transfer-data-plane:${edcVersion}")
-
-    implementation("${groupId}:data-plane-selector-api:${edcVersion}")
-    implementation("${groupId}:data-plane-selector-core:${edcVersion}")
-    implementation("${groupId}:data-plane-selector-client:${edcVersion}")
-
-    implementation("${groupId}:data-plane-api:${edcVersion}")
-    implementation("${groupId}:data-plane-core:${edcVersion}")
-    implementation("${groupId}:data-plane-http:${edcVersion}")
-
-    implementation("$groupId:vault-filesystem:$edcVersion")
-
-    api("$groupId:control-plane-spi:$edcVersion")
-    api("$groupId:data-plane-spi:$edcVersion")
-    */
 
     api(libs.edc.control.plane.spi)
     implementation(libs.edc.http)
@@ -91,16 +32,9 @@ dependencies {
     implementation(libs.edc.data.plane.selector.core)
     implementation(libs.edc.iam.mock)
 
-    //implementation(project(":playground:apiTest"))
-    //implementation(project(":transfer:transfer-07-provider-push-http:http-push-connector"))
-    //implementation(project(":newtransfer:transfer-07-provider-push-http:provider-push-http-backend-service"))
     implementation(project(":BlockchainCatalog:blockchain-catalog-api"))
     implementation(project(":BlockchainCatalog:blockchain-catalog-listener"))
     implementation(project(":blockchain-logger"))
-
-
-//    implementation(project(":transfer:StatusChecker"))
-//    implementation(project(":transfer:TransferFileLocal"))
 
     // provider push http example
     implementation(libs.edc.control.plane.core)
@@ -119,6 +53,9 @@ dependencies {
     implementation(libs.edc.data.plane.core)
     implementation(libs.edc.data.plane.http)
 
+    implementation(libs.opentelemetry.exporter.jaeger)
+    implementation(libs.edc.api.observability)
+    runtimeOnly(libs.edc.monitor.jdk.logger)
 }
 
 application {
@@ -129,4 +66,20 @@ tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
     exclude("**/pom.properties", "**/pom.xml")
     mergeServiceFiles()
     archiveFileName.set("consumer.jar")
+}
+
+val openTelemetry by configurations.creating
+
+dependencies {
+    openTelemetry(libs.opentelemetry)
+    openTelemetry(libs.opentelemetry.exporter.jaeger)
+}
+
+tasks.register("copyOpenTelemetryJar", Copy::class) {
+    from(openTelemetry)
+    into("build/libs")
+}
+
+tasks.build {
+    finalizedBy("copyOpenTelemetryJar")
 }
